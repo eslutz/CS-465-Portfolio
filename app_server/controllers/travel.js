33967c1baa702a3236eafe97a0df36201d398265
Eric Slutz <eric.slutz@icloud.com>
2023-07-21 22:32:33 -0400

    completed module 5 assignment

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,13 +1,55 @@
 const fs = require('fs');
 const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-const trips = JSON.parse(fs.readFileSync('app_server/data/trips.json', 'utf8'));
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
 
-/* GET travel view. */
-const travel = (req, res) => {
-    pageTitle = packageJson.description + ' | Travel';
-    res.render('travel', { activePage: 'travel', title: pageTitle, trips });
+/* Render travel list view */
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = packageJson.description + ' | dsaf adfsf as';
+
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'No trips found in database';
+        }
+    }
+
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody,
+        message
+    });
+};
+
+/* GET travel list. */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {}
+    };
+    console.log('>> travelController.travelList calling ' + requestOptions.url);
+    request(
+        requestOptions,
+        (err, {statusCode}, body) => {
+            let trips = [];
+            if (statusCode === 200 && body.length) {
+                trips = body.map((trip) => {
+                    trip.date = trip.date ? trip.date : 'N/A';
+                    return trip;
+                });
+            }
+            renderTravelList(req, res, trips);
+        }
+    );
 };
 
 module.exports = {
-    travel
+    travelList
 };
