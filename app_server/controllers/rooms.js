7904e151075f1a6c98604dc9d2a73177e63f1ec4
Eric Slutz <eric.slutz@icloud.com>
2023-08-02 10:46:40 -0400

    added app_server

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -5,7 +5,7 @@ const room = JSON.parse(fs.readFileSync('app_server/data/rooms.json', 'utf8'));
 /* GET rooms view. */
 const rooms = (req, res) => {
     pageTitle = packageJson.description + ' | Rooms';
-    res.render('rooms', { title: pageTitle, room });
+    res.render('rooms', { activePage: 'rooms', title: pageTitle, room });
 };
 
 module.exports = {
